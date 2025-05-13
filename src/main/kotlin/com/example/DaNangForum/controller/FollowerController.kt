925d8855b0c1d381c78c46a15b7e15f46ea67b32
trinhquyetchien2005 <trinhquyetchien2005@gmail.com>
2025-05-14 01:31:59 +0700

    fix(api): follower fix

--- src/main/kotlin/com/example/DaNangForum/controller/FollowerController.kt
+++ src/main/kotlin/com/example/DaNangForum/controller/FollowerController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -48,7 +49,7 @@ class FollowerController(val followerService: FollowerService) {
 
     @SecurityRequirement(name = "bearerAuth")
     @DeleteMapping("/unfollow")
-    fun unfollow(@RequestBody userId: Long): ResponseEntity<ApiResponse> {
+    fun unfollow(@RequestParam userId: Long): ResponseEntity<ApiResponse> {
         return followerService.unFollow(userId)
     }
 }
